2f803c16d5728e7cb0c19f48578dc164c71f1b19
Finotto <enrico.finotto@capgemini.com>
2023-12-21 12:40:25 +0100

    first commit

---
+++ src/main/java/com/student/controller/StudentController.java
@@ -0,0 +1,99 @@
+package com.student.controller;
+
+ 
+import java.net.URI;
+import java.util.Collection;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.student.StudentProperties;
+import com.student.core.College;
+import com.student.core.Student;
+import com.student.service.StudentService;
+ 
+@RestController
+@RequestMapping(value="/student")
+@CrossOrigin
+public class StudentController {
+	
+	@Inject
+	private StudentProperties studentProperties;
+	@Inject
+	private StudentService studentService;
+	
+	@GetMapping("/msg")
+	public String getMessage(@RequestHeader("user-agent") String userAgent) {
+		return studentProperties.getGreeting()+" "+userAgent;
+	}
+	@GetMapping
+	public Collection<Student> getAll() {
+		return studentService.getAllStudents();
+	}
+
+	@GetMapping("/{id}")
+	public Student getStudent(@PathVariable("id") long id) {
+		return studentService.get(id);
+	}
+	
+	@GetMapping(path="/single",
+		produces= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<Student> getSingleStudent(@RequestParam("id") Optional<Long> optional) {
+		return ResponseEntity.ok(studentService.get(optional.orElse(1l)));
+	}
+
+	
+	@GetMapping("/search/{department}")
+	public Collection<Student> getStudentsPerDepartment(@PathVariable("department") String department, @RequestParam("name")  Optional<String> optional) {
+		return studentService.getAllStudentsInDepartment(department, optional.orElse(""));
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> addStudent(@RequestBody Student student){
+		studentService.add(student);
+		if(student.getId() > 0) {
+			URI uri = URI.create("/college/student/" + student.getId());
+			return ResponseEntity.accepted().location(uri).build();
+		} else {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+	@PostMapping("/addStudent")
+	public ResponseEntity<Student> addPlusStudent(@RequestBody Student student){
+		studentService.add(student);
+		/*if(student.getId() > 0) {
+			URI uri = URI.create("/college/student/" + student.getId());
+			return ResponseEntity.accepted().location(uri).build();
+		} else {
+			return ResponseEntity.badRequest().build();
+		}*/
+		return ResponseEntity.ok(studentService.get(student.getId()));
+	}
+	
+	@PostMapping("/addCollege")
+	public void addPlusStudent(@RequestBody College student){
+		//studentService.add(student);
+		/*if(student.getId() > 0) {
+			URI uri = URI.create("/college/student/" + student.getId());
+			return ResponseEntity.accepted().location(uri).build();
+		} else {
+			return ResponseEntity.badRequest().build();
+		}*/
+		System.out.print("OK");
+	}
+}
---
+++ src/main/java/com/student/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.student.service;
+
+
+import java.util.Collection;
+
+import javax.inject.Inject;
+
+import com.student.core.Student;
+
+public interface StudentService {
+	@Inject
+	Student get(long id);
+	Collection<Student> getAllStudents();
+	Collection<Student> getAllStudentsInDepartment(String department, String lastNamelike);
+	void add(Student student);
+
+}
