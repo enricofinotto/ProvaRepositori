775da758436f73b1ea4aa00eea809c046d24c695
Finotto <enrico.finotto@capgemini.com>
2023-12-27 16:04:56 +0100

    corretto metodo update

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -15,9 +15,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -77,8 +79,8 @@ public class StudentController {
 		return "update completed successfully";
 	}*/
 	
-	@PostMapping("/updateStudent")
-	public Student updateStudent(@RequestBody Student student) {
-		return studentService.update(student);
+	@PutMapping("/updateStudent")
+	public void updateStudentSnd(@RequestBody Student student) {
+		studentService.update(student);
 	}
 }
